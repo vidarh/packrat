aa5ddd448a22cc217427f4a3d928bbac3bdb0f3d
Vidar Hokstad <vidar@hokstad.com>
2011-02-01 09:00:11 -0500

    Added initial header for dependency management API

---
+++ src/require.h
@@ -0,0 +1,45 @@
+#ifndef __PACKRAT_REQUIRE_H__
+#define __PACKRAT_REQUIRE_H__
+
+/**
+ *   Dependency management functions 
+ *
+ *   packrat_find_object(object, versionstring)
+ *     Checks whether the specificed object (given as a file name or package name)
+ *     exists, and matches the given version string.
+ *
+ *     If the object is a file, packrat employs the following methods to determine
+ *     the version number:
+ *     * First scan for $VER: 
+ *     * Failing that, scan for an RTC_MATCHWORD (romtag header for libraries etc.)
+ *     * Failing that, if running on AmigaOS/MOS/AROS, try calling OpenLibrary() and
+ *       examine the library base returned, if any.
+ *
+ *     FIXME: Consider whether to support querying rpm/dpkg when running on Linux,
+ *     at least to support build dependencies for cross compilation.
+ *
+ *   ## Object names ##
+ * 
+ *   * If the object name is a filesystem path, that path is considered first
+ *   * If the object matches a package name registered with Packrat, that package
+ *     is considered first
+ *   * If the name matches a file in Libs: or C: those paths are considered.
+ *   * Otherwise Packrat does not know the object. Future versions will attempt
+ *     remote lookups.
+ *
+ *   When running on non-Amiga compatible OS's, the following substitutions will
+ *   be made when checking paths: C: will be expanded to check /bin, /sbin, /usr/local/bin,
+ *   /usr/local/sbin and /opt/bin.
+ *   
+ *   FIXME: Consider if it's worth supporting checking for libraries on Linux too,
+ *   though that would require rewriting/mangling names much more.
+ *   
+ *   ## Version strings ##
+ * 
+ *   The comparison operators >,<,= will work as expected. When comparing dotted 
+ *   strings with numbers present, the numbers will be sorted numerically. E.g.
+ *   1.10 > 1.9 is _true_.
+ */
+
+
+#endif
