d4b9563e750ef7717d790444fa1fb9b9a552909a
Vidar Hokstad <vidar@hokstad.com>
2011-04-14 23:42:23 +0000

    Write build script to tmp dir

--- src/build.c
+++ src/build.c
@@ -1,14 +1,23 @@
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "packrat.h"
 #include "control.h"
 #include "require.h"
 #include "debug.h"
 
+#define PATHSIZE 256
+
+#ifdef __AMIGA__
+#define TMPPATH "T:"
+#else
+#define TMPPATH "/tmp/"
+#endif
+
 int packrat_build(const char * path)
 {
-  char lha[256]; 
+  char lha[PATHSIZE]; 
   struct packrat_control * ctrl = packrat_parse_control("../PACKAGE/control"); /* FIXME */
   if (!ctrl) {
 	fprintf(stderr,"FATAL: Unable to parse package control file\n");
@@ -20,7 +29,7 @@ int packrat_build(const char * path)
   /* We need c:lha to be able to pack the archive */
   if (!packrat_find_file("c:lha", lha, sizeof(lha))) {
 	fprintf(stderr,"FATAL: Packrat requires lha to be availble in C: (or $PATH on non-Amiga systems)\n");
-	return 1;
+	goto fail;
   }
 
   /* FIXME: Verify the PACKAGE directory */
@@ -32,13 +41,22 @@ int packrat_build(const char * path)
 	/* FIXME: Version check */
 	if (!packrat_find_object(node->str,PACKRAT_CMP_NONE,0)) {
 	  fprintf(stderr,"FATAL: Package requires '%s' for build\n", node->str);
-	  return 1;
+	  goto fail;
 	}
   }
 
   /* Copy the %%build section to T: w/ debug echo's */
+  char tmpfile[PATHSIZE];
+  /* FIXME: Create a unique path */
+  strncpy(tmpfile,TMPPATH"build",sizeof(tmpfile));
+  FILE * buildfile = fopen(tmpfile,"w");
+  if (!buildfile) {
+	fprintf(stderr,"FATAL: Unable to open temporary file for buid '%s'\n",tmpfile);
+	goto fail;
+  }
   FOREACH_ML(&(ctrl->build),node) {
-	fprintf(stderr,"BUILD: %s\n",node->str);
+	/* FIXME: Apply variable substitutions */
+	fprintf(buildfile,"%s\n",node->str);
   }
 
 
@@ -58,4 +76,8 @@ int packrat_build(const char * path)
   
   packrat_free_control(ctrl);
   return 0;
+
+ fail:
+  packrat_free_control(ctrl);
+  return 1;
 }
